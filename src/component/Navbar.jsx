dc4d1798819fb6a888df69cf3ac35603998b1e86
ucfangyiwang <wangfangyid1502@gmail.com>
2022-10-24 09:17:35 +1100

    update

---
+++ src/component/Navbar.jsx
@@ -0,0 +1,10 @@
+function Navbar() {
+  return (
+    <div className="w-screen h-12 bg-neutral-400">
+      <div className="flex justify-start content-center font-sans text-2xl font-bold text-yellow-400">
+        Crypto Tracking
+      </div>
+    </div>
+  );
+}
+export default Navbar;
