0521b5bf2f070067da0d3c3e805d885f723a39eb
Fangyi Wang <fangyiwang@Fangyis-MacBook-Air.local>
2022-10-31 14:58:31 +1100

    Updates

--- src/store/store.jsx
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import priceSlice from "./priceSlice";
-import favSlice from "./favSlice";
-export default configureStore({
-  reducer: {
-    price: priceSlice,
-    fav: favSlice,
-  },
-});
